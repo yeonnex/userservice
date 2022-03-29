fcf0274b8784294abc3aed50e5e6d3b247a802b4
yeonnex <82166132+yeonnex@users.noreply.github.com>
2022-03-29 14:51:11 +0900

    스프링 시큐리티 의존성 추가, 권한설정을 커스텀할 수 있게 configure(HttpSecurity http) 메서드 오버라이드 - class WebSecurity extends WebSecurityConfigurerAdapter - antMatchers("/users/**").permitAll(); - /users/** 이하의 경로는 권한 없이도 접근할 수 있음

---
+++ src/main/java/me/yeonnex/userservice/security/WebSecurity.java
@@ -0,0 +1,24 @@
+package me.yeonnex.userservice.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+    // Authentication
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    // Authorization
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("/users/**").permitAll();
+    }
+}
